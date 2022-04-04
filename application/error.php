596e4a1358fbf6d4a21bc5b4577219cedb10fc3d
znewbold <102979887+znewbold@users.noreply.github.com>
2022-04-04 11:23:16 -0400

    Update error.php

--- application/error.php
+++ application/error.php
@@ -18,7 +18,7 @@ IndexView::displayHeader($page_title);
 <table style = "width: 100%; border: none">
     <tr>
         <td style = "vertical-align: middle; text-align: center; width:100px">
-            <img src = '<?= BASE_URL ?>/www/img/error.jpg' style = "width: 80px; border: none"/>
+            <img src = '<?= BASE_URL ?>/www/images/error.jpg' style = "width: 80px; border: none"/>
         </td>
         <td style = "text-align: left; vertical-align: top;">
             <h3> Sorry, but an error has occurred.</h3>
